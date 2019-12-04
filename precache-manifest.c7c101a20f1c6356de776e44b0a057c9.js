2d58d725f94adbcf175a4e9472f11d422eda0e73
Max <dragoonz@yandex.ru>
2019-12-04 15:17:30 +0300

    Updates

---
+++ precache-manifest.c7c101a20f1c6356de776e44b0a057c9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aadfdcc5534f947ed2f983c0dc55d1c2",
+    "url": "/auto_test/index.html"
+  },
+  {
+    "revision": "ccab8857e0a37ea0afd5",
+    "url": "/auto_test/static/css/main.c9781528.chunk.css"
+  },
+  {
+    "revision": "85df213fdef0686c7d4e",
+    "url": "/auto_test/static/js/2.14fe99b0.chunk.js"
+  },
+  {
+    "revision": "ccab8857e0a37ea0afd5",
+    "url": "/auto_test/static/js/main.7f93f377.chunk.js"
+  },
+  {
+    "revision": "31545d4b72076c48e068",
+    "url": "/auto_test/static/js/runtime-main.85a89f19.js"
+  },
+  {
+    "revision": "28846795996807fd61042a3e3609e080",
+    "url": "/auto_test/static/media/Verdana-Bold.28846795.ttf"
+  },
+  {
+    "revision": "3ba52ab1fa0cd726e7868e9c6673902c",
+    "url": "/auto_test/static/media/Verdana.3ba52ab1.ttf"
+  },
+  {
+    "revision": "92f5c28027d4f642645aefd0aef23dc2",
+    "url": "/auto_test/static/media/arrow.92f5c280.svg"
+  },
+  {
+    "revision": "2d6939a168fc9cee21e654dbae077629",
+    "url": "/auto_test/static/media/arrowup.2d6939a1.svg"
+  },
+  {
+    "revision": "8587c4e495d2655c333291f211d15ab8",
+    "url": "/auto_test/static/media/logo.8587c4e4.svg"
+  },
+  {
+    "revision": "961f03f79808faf8e86f2f7c04a05d45",
+    "url": "/auto_test/static/media/plus.961f03f7.svg"
+  },
+  {
+    "revision": "8beb4b2d39596200d51099b52290cace",
+    "url": "/auto_test/static/media/success.8beb4b2d.svg"
+  },
+  {
+    "revision": "c1ae67a7f017170e3f2733b81a18f635",
+    "url": "/auto_test/static/media/trash.c1ae67a7.svg"
+  }
+]);
